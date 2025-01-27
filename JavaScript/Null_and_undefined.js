83131dd723d5bce3498d877cab011b3de9ff062c
Merajul Rahman <shiponrbm@gmail.com>
2025-01-27 16:44:50 +0600

    27jan25

--- JavaScript/Null_and_undefined.js
+++ JavaScript/Null_and_undefined.js
@@ -1,5 +1,6 @@
 //Null means there is no value or no object.
 //null is a way to reset a variable, signalling that it should not reference any object.
+
 //Whenever a variable is declared but not initialized or assigned a value, then it is stored as undefined. 
 // A function returns undefined if a value was not returned. 
 // A method or statement also returns undefined if the variable that is being evaluated does not have an assigned value.
